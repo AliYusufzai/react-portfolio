b13a450f2e0cc1ee6de77ace6e860c6317b42c14
Ali Raza Yusufzai <114583866+AliYusufzai@users.noreply.github.com>
2023-02-11 15:17:29 +0500

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,7 @@ import web1 from '../public/web1.png';
 import web2 from '../public/web2.png';
 import web3 from '../public/web3.png';
 import web4 from '../public/web4.png';
-import web5 from '../public/web5.png';
-import web6 from '../public/web6.png';
+
 
 export default function Home() {
 
@@ -79,9 +78,7 @@ export default function Home() {
             <div className='basis-1/3 flex-1'><a href='https://react-app-movieland.netlify.app/' target={'_blank'} rel={'noreferrer'}><Image src={web2} alt={"React movie app"} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout='responsive'/></a></div>
             <div className='basis-1/3 flex-1'><a href='https://react-deliciousss.netlify.app/' target={'_blank'} rel={'noreferrer'}><Image src={web3} alt={"React Recipe app"} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout='responsive'/></a></div>
             <div className='basis-1/3 flex-1'><Image src={web4} alt={"React Recipe app"} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout='responsive'/></div>
-            {/*<div className='basis-1/3 flex-1'><Image src={web5} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout='responsive'/></div>
-            <div className='basis-1/3 flex-1'><Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout='responsive'/></div>
- */}
+            
           </div>
         </section>
         
